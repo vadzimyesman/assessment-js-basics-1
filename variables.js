d1246e7242a53a8f31797f1c68d5688a46e97a76
Vadzim_Yesman <vvyesman@gmail.com>
2021-12-17 08:53:32 -0800

    variables

--- variables.js
+++ variables.js
@@ -1,17 +1,20 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+var myName = "Vadzim"; 
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+var favNum = 13;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+var lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +24,11 @@
 */
 
 //CODE HERE
+if (favNum==13){
+  console.log("lucky 13!")
+} else {
+  console.log("not lucky 13")
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +36,6 @@
 */
 
 //CODE HERE
+for (i=0;i<5;i++){
+  console.log(favNum)
+}
